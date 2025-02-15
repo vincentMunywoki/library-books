ef92129918a32ab61b87c434b279c875c4dba21c
vincent munywoki <87576661+vincentMunywoki@users.noreply.github.com>
2025-02-15 17:28:29 +0300

    Update books.tsx

--- client/src/pages/books.tsx
+++ client/src/pages/books.tsx
@@ -13,6 +13,7 @@ import { insertBookSchema, type Book } from "@shared/schema";
 import { apiRequest, queryClient } from "@/lib/queryClient";
 import { Search, Plus } from "lucide-react";
 
+
 export default function Books() {
   const [search, setSearch] = useState("");
   const { toast } = useToast();
